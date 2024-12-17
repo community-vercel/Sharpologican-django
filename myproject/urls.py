f930303c5d051745f5b15c30f0c1de819031a31a
anisulislam10 <anis.inbox10@gmail.com>
2024-12-17 10:52:35 +0000

    new

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,11 +18,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('shaarp3.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/', include('shaarp3.urls')),
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
