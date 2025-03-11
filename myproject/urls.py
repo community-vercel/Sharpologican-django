84411cb6c4e0d45ddc4be821db632d632b7f5246
community-vercel <community-vercel>
2025-03-11 09:23:51 -0700

    aaa

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/es/', include('   .urls')),
+    path('api/es/', include('Espan.urls')),
     path('api/fr/', include('France.urls')),
     path('api/nd/', include('Netherlandss.urls')),
     path('api/', include('shaarp3.urls')),
